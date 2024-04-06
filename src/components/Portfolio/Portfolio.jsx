cf636993b177c1845d89f0c39ff3806130cc3164
masonamc5 <145730456+masonamc5@users.noreply.github.com>
2024-04-06 16:19:41 -0600

    css and react for the portfolio page

---
+++ src/components/Portfolio/Portfolio.jsx
@@ -0,0 +1,15 @@
+
+import React from 'react';
+import './portfolio.css'; 
+import PortfolioGrid from '../PortfolioGrid/portfoliogrid'; 
+const Portfolio = () => {
+  return (
+    <div className="portfolio-page-container">
+    
+      <PortfolioGrid />
+      
+    </div>
+  );
+};
+
+export default Portfolio;
