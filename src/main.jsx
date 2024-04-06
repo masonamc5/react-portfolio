1752c366838a31ae23dc52a50bd08fbc44f5b851
masonamc5 <145730456+masonamc5@users.noreply.github.com>
2024-04-06 16:17:17 -0600

    main react

---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import './reset.css';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './app'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
+
+export default App;
