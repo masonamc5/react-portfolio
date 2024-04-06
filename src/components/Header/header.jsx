17787fab4c2db2533282a03ca0fec8c878ed3b65
masonamc5 <145730456+masonamc5@users.noreply.github.com>
2024-04-06 16:18:51 -0600

    css and react for the header

---
+++ src/components/Header/header.jsx
@@ -0,0 +1,16 @@
+//components/Header/Header.jsx
+import React from 'react';
+import Navigation from '../Navigation/navigation';
+import './header.css'; 
+
+function Header() {
+  return (
+    <header>
+      <h1>Mason McNeil</h1>
+      <Navigation />
+    </header>
+  );
+}
+
+export default Header;
+
